d70d870a633bdb2803df1dc27531bbbc2977121d
YouXam <youxam@outlook.com>
2024-07-13 17:06:29 +0800

    feat: synchronizing aimodel with Kubernetes
    Signed-off-by: YouXam <youxam@outlook.com>

--- app/model/api/models.ts
+++ app/model/api/models.ts
@@ -1,25 +1,44 @@
 'use server';
 
-import { v4 as uuidv4 } from 'uuid';
 import { z } from 'zod'
-import { AiModel } from '@/lib/prisma';
 import { revalidatePath } from 'next/cache';
-
 import prisma from '@/lib/prisma';
+import { applyAIModel, deleteAIModel } from '@/lib/k8s';
 
-const AddModelSchema = z.lazy(() => z.object({
+const AddModelSchema = z.object({
   name: z.string().min(1),
   type: z.enum(['remote', 'local']),
   modelName: z.string().min(1),
   baseUrl: z.string().optional(),
   apiToken: z.string().optional(),
   maxProcesses: z.number().min(1).optional(),
-}))
+})
 
 export type AddModelType = z.infer<typeof AddModelSchema>
 
+const IMAGE = 'youxam/uplion-aimodel-operator-test-worker:latest'
+
 export async function addModel(data_: Record<string, any>) {
   const data = AddModelSchema.parse(data_)
+
+  const existingModel = await prisma.aiModel.findFirst({
+    where: {
+      name: data.name
+    }
+  })
+
+  if (existingModel) {
+    throw new Error("Model already exists, try a different name")
+  }
+
+  await applyAIModel(data.name, {
+    type: data.type,
+    baseURL: data.baseUrl,
+    apiKey: data.apiToken,
+    model: data.modelName,
+    image: IMAGE,
+    maxProcessNum: data.maxProcesses || (data.type === 'local' ? 4 : 128)
+  })
 
   const newaimodel = await prisma.aiModel.create({ data: {
     ...data,
@@ -33,13 +52,26 @@ export async function addModel(data_: Record<string, any>) {
 export async function editModel(id: number, data_: Record<string, any>) {
   const data = AddModelSchema.parse(data_);
 
-  const token = await prisma.aiModel.findUnique({
+  const model = await prisma.aiModel.findUnique({
     where: { id: id }
   });
 
-  if (!token) {
+  if (!model) {
     throw new Error("Model not found");
   }
+
+  if (model.name != data.name) {
+    await deleteAIModel(model.name)
+  }
+
+  await applyAIModel(data.name, {
+    type: data.type,
+    baseURL: data.baseUrl,
+    apiKey: data.apiToken,
+    model: data.modelName,
+    image: IMAGE,
+    maxProcessNum: data.maxProcesses || (data.type === 'local' ? 4 : 128)
+  })
 
   await prisma.aiModel.update({
     where: { id: id },
---
+++ lib/k8s.ts
@@ -0,0 +1,96 @@
+import { KubeConfig, CustomObjectsApi } from '@kubernetes/client-node'
+
+
+
+const globalForK8s = global as unknown as { k8sApi: CustomObjectsApi }
+
+function getK8sApi() {
+  if (globalForK8s.k8sApi) return globalForK8s.k8sApi
+  const kc = new KubeConfig();
+  kc.loadFromDefault()
+  return kc.makeApiClient(CustomObjectsApi)
+}
+
+if (process.env.NODE_ENV !== 'production') globalForK8s.k8sApi = getK8sApi()
+
+type AIModelSpec = {
+  type: 'local' | 'remote'
+  model: string
+  msgBacklogThreshold?: number
+  replicas?: number
+  image: string
+  maxProcessNum: number
+
+  baseURL?: string
+  apiKey?: string
+}
+
+type AIModelConfig = {
+  apiVersion: "model.youxam.com/v1alpha1"
+  kind: "AIModel"
+  metadata: {
+    name: string
+  }
+  spec: AIModelSpec
+}
+
+interface K8sConfig {
+  metadata: {
+    name: string
+    resourceVersion?: string
+  }
+  spec: object
+}
+
+export async function applyAIModel(name: string, data: AIModelSpec) {
+  const body: AIModelConfig = {
+    apiVersion: "model.youxam.com/v1alpha1",
+    kind: "AIModel",
+    metadata: {
+      name: name + '-ai-model',
+    },
+    spec: data
+  }
+  await _applyAIModel('default', body)
+}
+
+export async function deleteAIModel(name: string) {
+  const k8sApi = getK8sApi()
+  try {
+    await k8sApi.deleteNamespacedCustomObject('model.youxam.com', 'v1alpha1', 'default', 'aimodels', name + '-ai-model')
+  } catch (e: any) {
+    if (e.response && e.response.statusCode === 404) {
+      console.log('CRD object not found');
+    } else {
+      console.error('Error deleting CRD object:', e);
+    }
+  }
+}
+
+async function _applyAIModel(namespace: string, body: AIModelConfig) {
+  await applyCustomResource('model.youxam.com', 'v1alpha1', namespace, 'aimodels', body)
+}
+
+async function applyCustomResource(group: string, version: string, namespace: string, plural: string, body: K8sConfig) {
+  const k8sApi = getK8sApi()
+  try {
+    const existing = await k8sApi.getNamespacedCustomObject(group, version, namespace, plural, body.metadata.name)
+    console.log('CRD object already exists, replacing');
+    const oldModel = existing.body as K8sConfig
+    body.metadata.resourceVersion = oldModel.metadata.resourceVersion
+    await k8sApi.replaceNamespacedCustomObject(group, version, namespace, plural, body.metadata.name, body)
+    console.log('CRD object replaced successfully');
+  } catch (e: any) {
+    if (e.response && e.response.statusCode === 404) {
+      console.log('CRD object not found, creating');
+      try {
+        await k8sApi.createNamespacedCustomObject(group, version, namespace, plural, body)
+        console.log('CRD object created successfully');
+      } catch (e: any) {
+        console.error('Error creating CRD object:', e);
+      }
+    } else {
+      console.error('Error applying CRD object:', e);
+    }
+  }
+}
